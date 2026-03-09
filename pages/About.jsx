bff55431347bb86d416f0c2d5ec5bf43f1d7ede5
DanielSahar <daniel.sahar@cycognito.com>
2026-03-09 20:31:24 +0200

    daniel about github testing

--- pages/About.jsx
+++ pages/About.jsx
@@ -3,5 +3,6 @@ export function About() {
     return <section className="container about">
         <h1>About Page</h1>
         <p>About Test</p>
+        <p>Daniel GitHub Test</p>
     </section>
 }
