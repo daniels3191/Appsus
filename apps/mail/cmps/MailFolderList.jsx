1b6ff0e373a4da744b1fee07c2f1914e32276118
Itay <itaymoyal31@gmail.com>
2026-03-10 18:00:28 +0200

    Added gmail logo and a little bit of css to it

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -1,8 +1,13 @@
-const { NavLink } = ReactRouterDOM
-
+const { NavLink, Link } = ReactRouterDOM
 export function MailFolderList() {
   return (
     <nav>
+      <Link to="/mail?status=inbox">
+        <div className="logo">
+          <img src="/apps/mail/icons/logo.svg" alt="" />
+          <span>Gmail</span>
+        </div>
+      </Link>
       <NavLink to="/mail?status=inbox">Inbox</NavLink>
       <NavLink to="/mail?status=sent">Sent</NavLink>
       <NavLink to="/mail?status=trash">Trash</NavLink>
