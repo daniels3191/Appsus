60ea71999156084ba6a690f1ce305335ee679bb9
DanielSahar <daniel.sahar@cycognito.com>
2026-03-18 22:43:35 +0200

    fix hover buttins styling

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,7 +2,5 @@
 export function About() {
     return <section className="container about">
         <h1>About Page</h1>
-        <p>About Test</p>
-        <p>Daniel GitHub Test</p>
     </section>
 }
